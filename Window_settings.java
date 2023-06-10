616f7d2b7b7c063ce193a768e30fb1310cac1ca1
abee02 <86598458+abee02@users.noreply.github.com>
2023-06-10 14:27:59 +0200

    Update Window_settings.java

--- Window_settings.java
+++ Window_settings.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 
 public class Window_settings {
 	
-	private static String FrameName = "Call of Soduko";
+	private static String FrameName = "Call of Sudoku";
 	//Changes Tab Icon and Name
 	public static void Tab(JFrame frame) {
 		ImageIcon ff = new ImageIcon("img/Firefighter-amico.png");
@@ -16,7 +16,7 @@ public class Window_settings {
 		//Closes the Game with Are you sure Message
         int result = JOptionPane.showOptionDialog(frame, "Do you really want to leave?", "Confirmation",
                 JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null,
-                new String[]{"Sorry Sir, that's my choice.", "No! That's a mistake touch!"}, null);
+                new String[]{"Yes", "No"}, null);
 
         if (result == JOptionPane.YES_OPTION) {
             frame.dispose();
