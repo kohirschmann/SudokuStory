c12dd3339cab420940797469d13586ed2bfffe52
Luyi Sun <132840065+LuyiSun02@users.noreply.github.com>
2023-06-11 12:44:02 +0200

    Add files via upload

--- Start.java
+++ Start.java
@@ -1,54 +1,155 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+
 public class Start {
-	
-	static ImageIcon Mike = new ImageIcon("img/Firefighter-pana.png");
-	
-	public static void main(String[] args) {
-		JFrame frame = new JFrame(Window_settings.getFrameName());
-		Window_settings.Tab(frame);
-		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		frame.setSize(1200, 900);
+
+    static class User {
+        static String username;
+
+        public static void changeUsername() {
+            UIManager.put("OptionPane.background", Color.WHITE);
+            UIManager.put("Panel.background", Color.WHITE);
+            ImageIcon ff = Scaling.Icon(new ImageIcon("img/Tiger-cage.png"));
+            Object input = JOptionPane.showInputDialog(null, "Welcome, what is your name?", "Choose Username", JOptionPane.PLAIN_MESSAGE, ff, null, null);
+            username = input.toString();
+
+            UIManager.put("OptionPane.background", null);
+            UIManager.put("Panel.background", null);
+        }
+
+        public static String getName() {
+            return username;
+        }
+    }
+
+    static ImageIcon tigerImage = new ImageIcon("img/Tiger-only.png");
+
+    public static void main(String[] args) {
+        User.changeUsername();
+        JFrame frame = new JFrame(Window_settings.getFrameName());
+        Window_settings.Tab(frame);
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.setSize(1200, 900);
+        frame.getContentPane().setBackground(new Color(160, 216, 144));
         frame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
-            	Window_settings.Close(frame);
+                Window_settings.Close(frame);
             }
         });
-        frame.setLayout(new FlowLayout());
-        //Objects on GUI
-        JLabel LMike = new JLabel(Scaling.Custom(Mike, 1000, 1000));
-        LMike.setLocation(300, 50);
-        
-        
-		JButton nGame = new JButton("New Game");
-		nGame.setPreferredSize(new Dimension(100, 30));
-		nGame.setLocation(100, 500);
-		nGame.addActionListener(new ActionListener() {
+        frame.setLayout(new BorderLayout());
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				SudokuUI_9x9.main(null);
-				frame.setVisible(false);
-				
-			}
-			
-		});
-		User.changeUsername();
-		frame.add(nGame);
-		frame.add(LMike);
-        frame.add(new Label("Hey "+ User.getName()+", so you are the new recruit for our Fire Department."));
-        frame.add(new Label("Nice to meet you! My Name is Mike and I will be your instructor from now on"));
+        // Greeting and Explanation
+        JPanel greetingPanel = new JPanel();
+        greetingPanel.setLayout(new BorderLayout());
+        greetingPanel.setBackground(new Color(160, 216, 144));
+
+        JLabel usernameLabel = new JLabel("Welcome, " + User.getName() + "!");
+        usernameLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        usernameLabel.setHorizontalAlignment(JLabel.CENTER);
+        greetingPanel.add(usernameLabel, BorderLayout.NORTH);
+
+        JLabel explanationLabel = new JLabel("<html><center>Introducing StorySudoku: Engage your mind and unlock captivating tales!<br>"
+                + "Immerse yourself in a unique puzzling experience that combines the timeless challenge of Sudoku with the thrilling progression of a story.<br>"
+                + "StorySudoku offers a twist to the classic game, where each solved sudoku unveils a part of an enchanting story.</center></html>");
+        explanationLabel.setFont(new Font("Arial", Font.PLAIN, 18));
+        explanationLabel.setHorizontalAlignment(JLabel.CENTER);
+        greetingPanel.add(explanationLabel, BorderLayout.CENTER);
+
+        frame.add(greetingPanel, BorderLayout.NORTH);
+
+        // Level Selection
+        JPanel levelPanel = new JPanel(new GridLayout(2, 5, 20, 20));
+        levelPanel.setBackground(new Color(160, 216, 144));
+        levelPanel.setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40));
+
+        // Button Images
+        JPanel levelPanel1 = new JPanel(new GridLayout(2, 5, 20, 20));
+        levelPanel1.setBackground(new Color(160, 216, 144));
+        levelPanel1.setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40));
+
+        // Button Images
+        ImageIcon image1 = new ImageIcon("img/Tiger-only.png");
+        ImageIcon image2 = new ImageIcon("img/image2.png");
+        ImageIcon image3 = new ImageIcon("img/image3.png");
+        ImageIcon image4 = new ImageIcon("img/image4.png");
+        ImageIcon image5 = new ImageIcon("img/image5.png");
+        ImageIcon image6 = new ImageIcon("img/image6.png");
+        ImageIcon image7 = new ImageIcon("img/image7.png");
+        ImageIcon image8 = new ImageIcon("img/image8.png");
+        ImageIcon image9 = new ImageIcon("img/image9.png");
+        ImageIcon image10 = new ImageIcon("img/image10.png");
+
+        ImageIcon[] buttonImages = { image1, image2, image3, image4, image5, image6, image7, image8, image9, image10 };
+
+        for (int i = 1; i <= 10; i++) {
+            JButton levelButton = new JButton();
+            levelButton.setFont(new Font("Arial", Font.BOLD, 20));
+            levelButton.setPreferredSize(new Dimension(200, 180));
+            levelButton.setBackground(new Color(81, 139, 61));
+            levelButton.setForeground(Color.WHITE);
+            levelButton.setBorder(BorderFactory.createLineBorder(new Color(56, 97, 43), 3));
+            levelButton.setFocusPainted(false);
+
+            if (i <= buttonImages.length) {
+                ImageIcon buttonImage = buttonImages[i - 1];
+                Image scaledImage = buttonImage.getImage().getScaledInstance(280, 280, Image.SCALE_SMOOTH);
+                ImageIcon scaledButtonImage = new ImageIcon(scaledImage);
+                levelButton.setIcon(scaledButtonImage);
+            }
+
+            JPanel buttonPanel = new JPanel(new BorderLayout());
+            buttonPanel.setOpaque(false);
+            JLabel levelLabel = new JLabel("Level " + i);
+            levelLabel.setFont(new Font("Arial", Font.BOLD, 18));
+            levelLabel.setHorizontalAlignment(JLabel.CENTER);
+            buttonPanel.add(levelLabel, BorderLayout.NORTH);
+            buttonPanel.add(levelButton, BorderLayout.CENTER);
+
+            levelButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    SudokuUI_4x4.main(null);
+                    frame.setVisible(false);
+                }
+            });
+            levelPanel1.add(buttonPanel);
+        }
+
+        frame.add(levelPanel1, BorderLayout.CENTER);
+
+
+        // Start New Game Button
+        JButton newGameButton = new JButton("Start a New Game");
+        newGameButton.setFont(new Font("Arial", Font.BOLD, 24));
+        newGameButton.setPreferredSize(new Dimension(300, 75));
+        newGameButton.setBackground(new Color(81, 139, 61));
+        newGameButton.setForeground(Color.WHITE);
+        newGameButton.setBorder(BorderFactory.createLineBorder(new Color(56, 97, 43), 3));
+        newGameButton.setFocusPainted(false);
+        newGameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SudokuUI_9x9.main(null);
+                frame.setVisible(false);
+            }
+        });
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBackground(new Color(160, 216, 144));
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(40, 0, 40, 0));
+        buttonPanel.add(newGameButton);
+
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+
         frame.setVisible(true);
-        
-	}
-	
-	
+    }
 }
+
+
+
+
+
+
+
